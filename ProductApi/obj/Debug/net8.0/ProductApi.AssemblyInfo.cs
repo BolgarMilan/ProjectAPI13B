2a5fa98d73c485d0adc78df6539e50583ad80e81
BolgarMilan <bolgarm@kkszki.hu>
2024-10-01 13:57:27 +0200

    Sql file with shop database and records are added.

--- ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
+++ ProductApi/obj/Debug/net8.0/ProductApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca9164b5d93ed1d894cff10305aeb6298d12a9b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
